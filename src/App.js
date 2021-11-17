475b5b5624f56d09ead67b67c4428ab652e4a733
khandelwalmohit <rockingmohitk@gmail.com>
2021-11-17 07:29:52 -0500

    foramt

--- src/App.js
+++ src/App.js
@@ -1,18 +1,14 @@
-import React from 'react'
-import './App.css';
-import Sidebar from './Sidebar';
+import React from "react";
+import "./App.css";
+import Sidebar from "./Sidebar";
 
 function App() {
   return (
     <div className="app">
-      
-
-      <div className = "app_body">
+      <div className="app_body">
         <Sidebar />
         {/*Chat*/}
       </div>
-
-
     </div>
   );
 }
