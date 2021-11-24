2e741364794d3857da20bb0e4410f4eb474cd85a
Snigdha Bhagat <snigdhabhagat21@gmail.com>
2021-11-23 22:29:29 -0500

    firebase start

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBqvzoGMxguVekozdTKob9hfLud5G9Zq3g",
+    authDomain: "chatapp-aaff0.firebaseapp.com",
+    projectId: "chatapp-aaff0",
+    storageBucket: "chatapp-aaff0.appspot.com",
+    messagingSenderId: "467126776530",
+    appId: "1:467126776530:web:84db55c7d836cd67fff30b"
+  };
